80a1f9c03b401cd66987f5d86c908f27de1111e2
sadewsunera6 <sadew101010@gmail.com>
2026-05-09 23:17:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?QWZQxF',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?gH95Kn',
     PORT: process.env.PORT || 8000,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
